9a1dd2579952f98aefc9e8782606dc0fcebecb3d
Hesham Massoud <hesham.massoud@commercetools.de>
2017-11-03 16:43:47 +0100

    CDL-2055: Enter CTP credentials through properties file as well as env vars.

--- src/main/java/de/coeur/sync/utils/SphereClientUtils.java
+++ src/main/java/de/coeur/sync/utils/SphereClientUtils.java
@@ -5,15 +5,16 @@ import com.commercetools.sync.commons.utils.ClientConfigurationUtils;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.client.SphereClientConfig;
 
+import javax.annotation.Nonnull;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static java.lang.String.format;
+
 public final class SphereClientUtils {
-    public static final SphereClientConfig CTP_SOURCE_CLIENT_CONFIG = SphereClientConfig.of(
-        System.getenv("COEUR_SOURCE_PROJECT_KEY"),
-        System.getenv("COEUR_SOURCE_CLIENT_ID"),
-        System.getenv("COEUR_SOURCE_CLIENT_SECRET"));
-    public static final SphereClientConfig CTP_TARGET_CLIENT_CONFIG = SphereClientConfig.of(
-        System.getenv("COEUR_TARGET_PROJECT_KEY"),
-        System.getenv("COEUR_TARGET_CLIENT_ID"),
-        System.getenv("COEUR_TARGET_CLIENT_SECRET"));
+    private static final String CTP_CREDENTIALS_PROPERTIES = "ctp.credentials.properties";
+    private static final SphereClientConfig CTP_SOURCE_CLIENT_CONFIG = getCtpSourceClientConfig();
+    private static final SphereClientConfig CTP_TARGET_CLIENT_CONFIG = getCtpTargetClientConfig();
     public static final SphereClient CTP_SOURCE_CLIENT =
         ClientConfigurationUtils.createClient(CTP_SOURCE_CLIENT_CONFIG);
     public static final SphereClient CTP_TARGET_CLIENT =
@@ -22,5 +23,31 @@ public final class SphereClientUtils {
     public static void closeCtpClients() {
         CTP_SOURCE_CLIENT.close();
         CTP_TARGET_CLIENT.close();
+    }
+
+    private static SphereClientConfig getCtpSourceClientConfig() {
+        return getCtpClientConfig("source.", "COEUR_SOURCE");
+    }
+
+    private static SphereClientConfig getCtpTargetClientConfig() {
+        return getCtpClientConfig("target.", "COEUR_TARGET");
+    }
+
+    private static SphereClientConfig getCtpClientConfig(@Nonnull final String propertiesPrefix,
+                                                         @Nonnull final String envVarPrefix) {
+        try {
+            final InputStream propStream = SphereClientUtils.class.getClassLoader()
+                                                                  .getResourceAsStream(CTP_CREDENTIALS_PROPERTIES);
+            if (propStream != null) {
+                final Properties ctpCredsProperties = new Properties();
+                ctpCredsProperties.load(propStream);
+                return SphereClientConfig.ofProperties(ctpCredsProperties, propertiesPrefix);
+            }
+        } catch (Exception exception) {
+            throw new IllegalStateException(format("CTP credentials file \"%s\" found, but CTP properties"
+                + " for prefix \"%s\" can't be read", CTP_CREDENTIALS_PROPERTIES, propertiesPrefix), exception);
+        }
+
+        return SphereClientConfig.ofEnvironmentVariables(envVarPrefix);
     }
 }
