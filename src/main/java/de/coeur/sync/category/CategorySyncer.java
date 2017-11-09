bfc90dd5829f4075cdafba09c0a668904852ba1f
Hesham Massoud <hesham.massoud@commercetools.de>
2017-11-09 10:19:17 +0100

    CDL-2055: Sync categories in batches, where each batch is a category page.

--- src/main/java/de/coeur/sync/category/CategorySyncer.java
+++ src/main/java/de/coeur/sync/category/CategorySyncer.java
@@ -3,11 +3,10 @@ package de.coeur.sync.category;
 import com.commercetools.sync.categories.CategorySync;
 import com.commercetools.sync.categories.CategorySyncOptions;
 import com.commercetools.sync.categories.CategorySyncOptionsBuilder;
+import com.commercetools.sync.categories.helpers.CategorySyncStatistics;
 import com.commercetools.sync.commons.helpers.BaseSyncStatistics;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.coeur.sync.services.CategoryService;
-import de.coeur.sync.services.impl.CategoryServiceImpl;
 import io.sphere.sdk.categories.Category;
 import io.sphere.sdk.categories.CategoryDraft;
 import org.slf4j.Logger;
@@ -15,8 +14,11 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import java.util.List;
+import java.util.concurrent.CompletionStage;
 
+import static com.commercetools.sync.categories.utils.CategoryReferenceReplacementUtils.buildCategoryQuery;
 import static com.commercetools.sync.categories.utils.CategoryReferenceReplacementUtils.replaceCategoriesReferenceIdsWithKeys;
+import static com.commercetools.sync.commons.utils.CtpQueryUtils.queryAll;
 import static de.coeur.sync.utils.SphereClientUtils.CTP_SOURCE_CLIENT;
 import static de.coeur.sync.utils.SphereClientUtils.CTP_TARGET_CLIENT;
 import static de.coeur.sync.utils.SphereClientUtils.closeCtpClients;
@@ -24,6 +26,7 @@ import static java.lang.String.format;
 
 public class CategorySyncer {
     private static final Logger LOGGER = LoggerFactory.getLogger(CategorySyncer.class);
+    private static CategorySync categorySync;
 
     /**
      * Sync runner..
@@ -31,14 +34,12 @@ public class CategorySyncer {
      * @param args all args
      */
     public static void main(final String[] args) {
-        final CategorySync sync = setupSync();
-        final List<CategoryDraft> categoryDrafts = getSourceCategoryDraftsWithReferencesAsKeys();
-
+        categorySync = setupSync();
         LOGGER.info("Starting sync..");
-        sync.sync(categoryDrafts)
-            .thenAccept(statistics -> {
+        syncCategoryPages()
+            .thenAccept(categorySyncStatistics -> {
                 try {
-                    final String statisticsAsJSONString = getStatisticsAsJSONString(statistics);
+                    final String statisticsAsJSONString = getStatisticsAsJSONString(categorySyncStatistics);
                     LOGGER.info(statisticsAsJSONString);
                 } catch (final JsonProcessingException exception) {
                     LOGGER.error("Invalid statistics JSON string..", exception);
@@ -66,16 +67,29 @@ public class CategorySyncer {
     }
 
     /**
-     * // TODO: Instead of reference expansion, we could cache all keys and replace references manually.
-     * Fetches the {@code CTP_SOURCE_CLIENT} project categories with references expanded. Then it replaces all the
-     * references with the keys and returns the reference replaced category drafts which are ready to sync.
+     * Fetches the {@code CTP_SOURCE_CLIENT} project categories with all needed references expanded and treats category
+     * page as a batch to the sync process. Then returns a completion stage containing the sync process statistics as a
+     * result.
      *
-     * @return category drafts which are exactly the categories but references replaces with keys.
+     * @return completion stage containing the sync process statistics as a result.
      */
-    private static List<CategoryDraft> getSourceCategoryDraftsWithReferencesAsKeys() {
-        final CategoryService categoryService = new CategoryServiceImpl(CTP_SOURCE_CLIENT);
-        final List<Category> categories = categoryService.fetchAll();
-        return replaceCategoriesReferenceIdsWithKeys(categories);
+    private static CompletionStage<CategorySyncStatistics> syncCategoryPages() {
+        return queryAll(CTP_SOURCE_CLIENT, buildCategoryQuery(), CategorySyncer::syncCategoryPage)
+            .thenApply(voidResult -> categorySync.getStatistics());
+    }
+
+    /**
+     * // TODO: Instead of reference expansion, we could cache all keys and replace references manually.
+     * Given a {@link List} representing a page of {@link Category}, this method replaces all the
+     * references with the keys and returns the reference replaced category drafts which are ready to sync. Then calls
+     * (in a blocking fashion) the sync process on these reference replaced category drafts.
+     *
+     */
+    private static void syncCategoryPage(@Nonnull final List<Category> categoryPage) {
+        final List<CategoryDraft> draftsWithKeysInReferences = replaceCategoriesReferenceIdsWithKeys(categoryPage);
+        categorySync.sync(draftsWithKeysInReferences)
+                    .toCompletableFuture()
+                    .join();
     }
 
     /**
