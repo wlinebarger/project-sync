b8f773b3652f3b6874e4c624e1ebf52c0c646aad
Hesham Massoud <hesham.massoud@commercetools.de>
2018-01-04 14:07:38 +0100

    #3: Add missing test for logStatistics.

--- src/test/java/de/coeur/sync/utils/StatisticsUtilsTest.java
+++ src/test/java/de/coeur/sync/utils/StatisticsUtilsTest.java
@@ -5,13 +5,35 @@ import com.commercetools.sync.commons.helpers.BaseSyncStatistics;
 import com.commercetools.sync.products.helpers.ProductSyncStatistics;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.Test;
+import uk.org.lidalia.slf4jext.Level;
+import uk.org.lidalia.slf4jtest.LoggingEvent;
+import uk.org.lidalia.slf4jtest.TestLogger;
+import uk.org.lidalia.slf4jtest.TestLoggerFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StatisticsUtilsTest {
+    private static final TestLogger testLogger = TestLoggerFactory.getTestLogger(StatisticsUtils.class);
     private BaseSyncStatistics syncStatistics;
 
-    // TODO: NEED TO TEST #logStatistics
+    @Test
+    public void logStatistics_WithProductSyncStatistics_ShouldLogStatistics() throws JsonProcessingException {
+        syncStatistics = new ProductSyncStatistics();
+        syncStatistics.incrementCreated(10);
+        syncStatistics.incrementFailed(10);
+        syncStatistics.incrementUpdated(10);
+        syncStatistics.incrementProcessed(30);
+
+        final String statisticsAsJSONString = StatisticsUtils.getStatisticsAsJSONString(syncStatistics);
+
+        StatisticsUtils.logStatistics(syncStatistics, testLogger);
+
+        assertThat(testLogger.getAllLoggingEvents()).hasSize(1);
+        final LoggingEvent loggingEvent = testLogger.getAllLoggingEvents().get(0);
+        assertThat(loggingEvent).isExactlyInstanceOf(LoggingEvent.class);
+        assertThat(loggingEvent.getLevel()).isEqualTo(Level.INFO);
+        assertThat(loggingEvent.getMessage()).contains(statisticsAsJSONString);
+    }
 
 
     @Test
